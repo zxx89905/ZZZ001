d737d0e3cee6ce2098c84567b4125a5ad5eecfbd
zxx89905 <164715034+zxx89905@users.noreply.github.com>
2026-04-25 15:23:26 +0800

    Update build configuration and rebuild

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ZZZ001/'
+  base: '/ZZZ001/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  }
 })
